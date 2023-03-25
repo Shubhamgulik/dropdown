93625209b307e60c741956e10b2bafe35b183773
Shubhamgulik <42814032+Shubhamgulik@users.noreply.github.com>
2023-03-25 14:56:24 +0530

    Cleaning code and adding comments

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Dropdown from "./Dropdown";
 function App() {
+  // List of subjects passed to the Dropdown component
   const list = ["Java", "C++", "Python", "Javascript", "NodeJS"];
   return (
     <div className="App">
+      {/* Passed list props to Dropdown component */}
       <Dropdown list={list} />
     </div>
   );
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,7 @@
+// importing useState hook
 import React, { useState } from "react";
+
+// importing css file
 import "./Dropdown.css";
 
 function Dropdown({ list }) {
@@ -11,25 +14,23 @@ function Dropdown({ list }) {
     setSelectItem(e.target.innerHTML);
     setHover(false);
   };
+
   return (
     <div className="dropdown">
-      <h2 className="header">Select a topic?</h2>
-      <div
-        className="dropdown__container"
-        onMouseOver={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-      >
-        <div className="dropdown__header">
+      <h2 className="header">Select language?</h2>
+      <div className="dropdown__container" onMouseLeave={() => setHover(false)}>
+        <div className="dropdown__header" onMouseOver={() => setHover(true)}>
           <p className="dropdown__selectItem">{selectItem}</p>
         </div>
-        <div className={`dropdown__list ${hover ? null : "dropdown__show"}`}>
-          {list.map((item) => {
-            return (
-              <p onClick={handleClick} className="dropdown__item">
-                {item}
-              </p>
-            );
-          })}
+        <div className="dropdown__list">
+          {hover &&
+            list.map((item) => {
+              return (
+                <p onClick={handleClick} className="dropdown__item">
+                  {item}
+                </p>
+              );
+            })}
         </div>
       </div>
     </div>
